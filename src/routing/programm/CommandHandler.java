41fac2a12976342d9ca9ab9657bfbad6b49e611a
uktup <uktup@kit.edu>
2024-09-19 16:43:16 +0200

    First commit

--- src/routing/programm/CommandHandler.java
+++ src/routing/programm/CommandHandler.java
@@ -75,11 +75,11 @@ public class CommandHandler {
     }
     private boolean errorHandler(Command commandExecutor, String[] arguments) {
         if (!commandExecutor.availability()) {
-            System.out.printf("ERROR_COMMAND_AVAILABILITY %s.", commandExecutor.getClass().getSimpleName());
+            System.out.printf("Error, COMMAND_AVAILABILITY %s.", commandExecutor.getClass().getSimpleName());
             return false;
         }
         if (!commandExecutor.validArguments(arguments)) {
-            System.out.printf("ERROR_COMMAND_ARGUMENTS %s.", commandExecutor.getClass().getSimpleName());
+            System.out.printf("Error, COMMAND_ARGUMENTS %s.", commandExecutor.getClass().getSimpleName());
             return false;
         }
         return true;
