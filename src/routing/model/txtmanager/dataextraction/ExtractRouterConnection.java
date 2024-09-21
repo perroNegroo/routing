1e7c5a07dd9f5f22ef072feca516573520bae317
uktup <uktup@kit.edu>
2024-09-21 18:23:10 +0200

    First commit

--- src/routing/model/txtmanager/dataextraction/ExtractRouterConnection.java
+++ src/routing/model/txtmanager/dataextraction/ExtractRouterConnection.java
@@ -3,8 +3,6 @@ package routing.model.txtmanager.dataextraction;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
 
 import static routing.model.txtmanager.FileToList.fileToList;
 
@@ -13,7 +11,6 @@ import static routing.model.txtmanager.FileToList.fileToList;
  * @author uktup
  */
 public final class ExtractRouterConnection {
-    //private static final Pattern ROUTER_EDGE_PATTERN = Pattern.compile("(\\w+_Router)\\s<-->\\s(\\w+_Router)");
     private ExtractRouterConnection() { }
     /**
      * Extracts router-to-router edges from the specified file.
