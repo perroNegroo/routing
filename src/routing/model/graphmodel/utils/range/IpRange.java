b1e65b6fb245959e4a300f0dfcfa00873e7e3e26
uktup <uktup@kit.edu>
2024-09-22 22:15:44 +0200

    First commit

--- src/routing/model/graphmodel/utils/range/IpRange.java
+++ src/routing/model/graphmodel/utils/range/IpRange.java
@@ -11,19 +11,19 @@ public final class IpRange {
      /**
      * Checks if two CIDR ranges are disjoint.
      *
-     * @param cidr1 the first CIDR notation (e.g., "192.168.1.0/24")
-     * @param cidr2 the second CIDR notation (e.g., "10.0.0.0/8")
+     * @param firstCidr the first CIDR notation
+     * @param secondCidr the second CIDR notation
      * @return true if the two ranges are disjoint, false otherwise
      */
-    public static boolean areDisjoint(String cidr1, String cidr2) {
-        int[] range1 = calculateRange(cidr1);
-        int start1 = range1[0];
-        int end1 = range1[1];
+    public static boolean areDisjoint(String firstCidr, String secondCidr) {
+        int[] firstRange = calculateRange(firstCidr);
+        int firstLowerBound = firstRange[0];
+        int firstHighBound = firstRange[1];
 
-        int[] range2 = calculateRange(cidr2);
-        int start2 = range2[0];
-        int end2 = range2[1];
+        int[] secondRange = calculateRange(secondCidr);
+        int secondLowerBound = secondRange[0];
+        int secondHigherBound = secondRange[1];
 
-        return (end1 < start2 || end2 < start1);
+        return (firstHighBound < secondLowerBound || secondHigherBound < firstLowerBound);
     }
 }
