2a91cbd6489c47cc88cb0452e43cc4eb383a7376
uktup <uktup@kit.edu>
2024-09-23 14:13:09 +0200

    First commit

--- src/routing/programm/commands/SendPackage.java
+++ src/routing/programm/commands/SendPackage.java
@@ -16,6 +16,7 @@ import static routing.model.graphmodel.utils.NetworkIdentifier.findNetworkForIP;
  */
 public class SendPackage implements Command {
     private static final String PATH_DELIMITER = " ";
+    private static final String ERROR_MESSAGE_NO_PATH = "Error, there is no path between %s and %s.%n";
     @Override
     public void execute(String[] arguments) {
         String firstIp = arguments[0];
@@ -26,10 +27,10 @@ public class SendPackage implements Command {
         if (Objects.equals(firstNetwork.getIpV4(), secondNetwork.getIpV4())) {
             path = firstNetwork.getNode(firstIp).getShortestWays(destinationIp);
             if (path == null) {
-                System.out.printf("Error, there is no path between %s and %s.%n", firstIp, destinationIp);
-                return;
+                System.out.printf(ERROR_MESSAGE_NO_PATH, firstIp, destinationIp);
+            } else {
+                System.out.println(String.join(PATH_DELIMITER, path));
             }
-            System.out.println(String.join(PATH_DELIMITER, path));
             return;
         }
 
@@ -37,15 +38,12 @@ public class SendPackage implements Command {
         List<String> routerToRouter = firstNetwork.getRouter().getShortestInterWays(secondNetwork.getRouter().getIpV4());
         List<String> routerToDestination = secondNetwork.getNode(secondNetwork.getRouter().getIpV4()).getShortestWays(destinationIp);
         if (firstToRouter == null || routerToRouter == null || routerToDestination == null) {
-            System.out.printf("Error, there is no path between %s and %s.%n", firstIp, destinationIp);
+            System.out.printf(ERROR_MESSAGE_NO_PATH, firstIp, destinationIp);
             return;
         }
         path.addAll(firstToRouter);
         path.addAll(routerToRouter);
         path.addAll(routerToDestination);
-        //path.addAll(firstNetwork.getNode(firstIp).getShortestWays(firstNetwork.getRouter().getIpV4()));
-        //path.addAll(firstNetwork.getRouter().getShortestInterWays(secondNetwork.getRouter().getIpV4()));
-        //path.addAll(secondNetwork.getNode(secondNetwork.getRouter().getIpV4()).getShortestWays(destinationIp));
         System.out.println(String.join(PATH_DELIMITER, new LinkedHashSet<>(path)));
 
 
